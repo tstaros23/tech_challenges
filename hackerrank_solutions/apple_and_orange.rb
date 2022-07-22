663ec83a97fb5c2912523266bea281ee8650c8db
Ted Staros <tstaros23@gmail.com>
2022-07-22 17:37:25 -0600

    Work in progress

---
+++ hackerrank_solutions/apple_and_orange.rb
@@ -0,0 +1,28 @@
+class Algorithm
+  def countApplesAndOranges(s, t, a, b, apples, oranges)
+
+
+  end
+  test = Algorithm.countApplesAndOranges()
+end
+#
+# Complete the 'countApplesAndOranges' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER s
+#  2. INTEGER t
+#  3. INTEGER a
+#  4. INTEGER b
+#  5. INTEGER_ARRAY apples
+#  6. INTEGER_ARRAY oranges
+#
+
+#so we have an array. [s,t] is the range where the house lies
+#a is the where the apply tree lies on the array for exsmple
+# we have an array of apples that include the distance they are away from the applie tree
+# a = 4, s = 7, t = 10
+# apple array = [2,3,-4]
+# create an array from a range for the house
+# iterate through apple array and add each value to the apple tree value.
+# then  compare it to the house array and count each time a value in the apple array is in the range
+#
